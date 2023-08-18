75c5e6143980626c1bfdfa96a7886394eaa4d991
IYarosh <IYarosh@qulix.com>
2023-08-18 15:34:32 +0300

    Excercise 4. Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,6 +3,8 @@ import { Twitter, Facebook } from 'react-feather';
 import styled from 'styled-components/macro';
 import MaxWidthWrapper from '../MaxWidthWrapper';
 
+import { QUERIES } from '../../constants';
+
 import VisuallyHidden from '../VisuallyHidden';
 
 const Footer = () => {
@@ -144,6 +146,16 @@ const TopRow = styled.div`
   font-size: 0.875rem;
   border-bottom: 1px solid var(--color-gray-700);
   padding: 24px 0;
+
+  @media ${QUERIES.tabletAndUp} {
+    flex-direction: row;
+    justify-content: center;
+    gap: 24px;
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    justify-content: flex-end;
+  }
 `;
 
 const Social = styled.div`
@@ -170,6 +182,17 @@ const MainNavArea = styled.div`
   gap: 32px;
   padding: 32px 0 48px;
   text-align: center;
+
+  @media ${QUERIES.tabletAndUp} {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(190px, 1fr));
+    gap: 20px;
+    text-align: left;
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    grid-template-columns: repeat(4, minmax(190px, 1fr));
+  }
 `;
 
 const MainNavHeading = styled.h2`
@@ -196,6 +219,10 @@ const Subfooter = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  @media ${QUERIES.laptopAndUp} {
+    align-items: start;
+  }
 `;
 
 const Logo = styled.a`
